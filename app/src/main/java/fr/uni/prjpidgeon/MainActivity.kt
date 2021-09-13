76038b38e27a947a9c00fd5775b8c9508b20c75a
riccardo <rikilnl@gmail.com>
2021-09-13 17:12:44 +0200

    Adding compass

--- app/src/main/java/fr/uni/prjpidgeon/MainActivity.kt
+++ app/src/main/java/fr/uni/prjpidgeon/MainActivity.kt
@@ -10,9 +10,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import fr.uni.prjpidgeon.databinding.ActivityMainBinding
-import kotlin.math.asin
+import kotlin.math.*
 import kotlin.math.atan
-import kotlin.math.sin
+import android.R
+
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
@@ -22,8 +26,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var sensorManager: SensorManager
     private val accelerometerReading = FloatArray(3)
     private val magnetometerReading = FloatArray(3)
-    private val rotationMatrix = FloatArray(9)
-    private val orientationAngles = FloatArray(3)
+    private var estPitch = 0f
+    private var estRoll = 0f
+    private var estYaw = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,50 +67,55 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        // copy values to class properties
         if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
-            var pitchEstimate = asin(event.values[0]/9.81f)
-            var rollEstimate = atan(event.values[1]/event.values[2])
+            estPitch = asin(event.values[0]/9.81f)
+            estRoll = atan(event.values[1]/event.values[2])
 
-            binding.pitch.text = pitchEstimate.toString()
-            binding.roll.text = rollEstimate.toString()
+            binding.pitch.text = estPitch.toString()
+            binding.roll.text = estRoll.toString()
 
             binding.xAxis.text = event.values[0].toString()
             binding.yAxis.text = event.values[1].toString()
             binding.zAxis.text = event.values[2].toString()
 
-            ObjectAnimator.ofFloat(binding.centerCross, "translationX", + pitchEstimate * 200).apply {
-                start()
-            }
-            ObjectAnimator.ofFloat(binding.centerCross, "translationY", - rollEstimate * 200).apply {
-                start()
-            }
+            ObjectAnimator.ofFloat(
+                binding.centerCross,
+                "translationX",
+                + estPitch * 200
+            ).apply { start() }
 
+            ObjectAnimator.ofFloat(
+                binding.centerCross,
+                "translationY",
+                - estRoll * 200
+            ).apply { start() }
 
         } else if (event?.sensor?.type == Sensor.TYPE_MAGNETIC_FIELD) {
-            System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
+            val xmag = event.values[0]
+            val ymag = event.values[1]
+            val zmag = event.values[2]
+
+            estYaw = atan2(-ymag, xmag)
+            /*estYaw =  when {
+                xmag < 0f -> (180f - atan(xmag / ymag))
+                xmag > 0f && ymag < 0f -> -atan(ymag/xmag)
+                xmag > 0f && ymag > 0f -> 360f - atan(ymag/xmag)
+                xmag == 0f && ymag < 0f -> 90f
+                else -> 270f
+            }*/
+
+            binding.yaw.text = estYaw.toString()
+
+            ObjectAnimator.ofFloat(
+                binding.compass,
+                "rotation",
+                -(estYaw / PI * 180f).toFloat()).apply { start() }
         }
     }
 
     override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
         // Do something here if sensor accuracy changes.
         // You must implement this callback in your code.
-    }
-
-    fun updateOrientationAngles() {
-        // Update rotation matrix, which is needed to update orientation angles.
-        SensorManager.getRotationMatrix(
-            rotationMatrix,
-            null,
-            accelerometerReading,
-            magnetometerReading
-        )
-
-        // "rotationMatrix" now has up-to-date information.
-
-        SensorManager.getOrientation(rotationMatrix, orientationAngles)
-
-        // "orientationAngles" now has up-to-date information.
     }
 
 
